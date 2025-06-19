d1cd960d40e8b5e1cd4ad08a9c713be85e2a8b78
milo <anthanielaniel05@gmail.com>
2025-06-20 01:31:31 +0800

    huhu

---
+++ D032310396/D032310396/D032310396/FormStock.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace D032310396
+{
+    partial class FormStock
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxQuantity = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonBook = new System.Windows.Forms.Button();
+            this.buttonAuthor = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.textBoxISBN = new System.Windows.Forms.TextBox();
+            this.textBoxAuthorID = new System.Windows.Forms.TextBox();
+            this.textBoxStockID = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ISBN = new System.Windows.Forms.Label();
+            this.dataGridViewStock = new System.Windows.Forms.DataGridView();
+            this.textBoxDateRecorded = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStock)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxQuantity
+            // 
+            this.textBoxQuantity.Location = new System.Drawing.Point(317, 335);
+            this.textBoxQuantity.Name = "textBoxQuantity";
+            this.textBoxQuantity.Size = new System.Drawing.Size(127, 22);
+            this.textBoxQuantity.TabIndex = 39;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(199, 335);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 16);
+            this.label1.TabIndex = 38;
+            this.label1.Text = "QuantityInStock";
+            // 
+            // buttonBook
+            // 
+            this.buttonBook.Location = new System.Drawing.Point(614, 319);
+            this.buttonBook.Name = "buttonBook";
+            this.buttonBook.Size = new System.Drawing.Size(90, 38);
+            this.buttonBook.TabIndex = 37;
+            this.buttonBook.Text = "Form Book";
+            this.buttonBook.UseVisualStyleBackColor = true;
+            this.buttonBook.Click += new System.EventHandler(this.buttonBook_Click);
+            // 
+            // buttonAuthor
+            // 
+            this.buttonAuthor.Location = new System.Drawing.Point(614, 254);
+            this.buttonAuthor.Name = "buttonAuthor";
+            this.buttonAuthor.Size = new System.Drawing.Size(99, 43);
+            this.buttonAuthor.TabIndex = 36;
+            this.buttonAuthor.Text = "Form Author";
+            this.buttonAuthor.UseVisualStyleBackColor = true;
+            this.buttonAuthor.Click += new System.EventHandler(this.buttonAuthor_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(464, 415);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(75, 23);
+            this.buttonDelete.TabIndex = 35;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Location = new System.Drawing.Point(291, 415);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(75, 23);
+            this.buttonUpdate.TabIndex = 34;
+            this.buttonUpdate.Text = "Update";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(111, 415);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 33;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            // 
+            // textBoxISBN
+            // 
+            this.textBoxISBN.Location = new System.Drawing.Point(317, 294);
+            this.textBoxISBN.Name = "textBoxISBN";
+            this.textBoxISBN.Size = new System.Drawing.Size(127, 22);
+            this.textBoxISBN.TabIndex = 32;
+            // 
+            // textBoxAuthorID
+            // 
+            this.textBoxAuthorID.Location = new System.Drawing.Point(317, 243);
+            this.textBoxAuthorID.Name = "textBoxAuthorID";
+            this.textBoxAuthorID.Size = new System.Drawing.Size(127, 22);
+            this.textBoxAuthorID.TabIndex = 31;
+            // 
+            // textBoxStockID
+            // 
+            this.textBoxStockID.Location = new System.Drawing.Point(317, 202);
+            this.textBoxStockID.Name = "textBoxStockID";
+            this.textBoxStockID.Size = new System.Drawing.Size(127, 22);
+            this.textBoxStockID.TabIndex = 30;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(240, 294);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 16);
+            this.label3.TabIndex = 29;
+            this.label3.Text = "ISBN";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(240, 243);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 16);
+            this.label2.TabIndex = 28;
+            this.label2.Text = "AuthorID";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // ISBN
+            // 
+            this.ISBN.AutoSize = true;
+            this.ISBN.Location = new System.Drawing.Point(240, 202);
+            this.ISBN.Name = "ISBN";
+            this.ISBN.Size = new System.Drawing.Size(54, 16);
+            this.ISBN.TabIndex = 27;
+            this.ISBN.Text = "StockID";
+            // 
+            // dataGridViewStock
+            // 
+            this.dataGridViewStock.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewStock.Location = new System.Drawing.Point(96, 33);
+            this.dataGridViewStock.Name = "dataGridViewStock";
+            this.dataGridViewStock.RowHeadersWidth = 51;
+            this.dataGridViewStock.RowTemplate.Height = 24;
+            this.dataGridViewStock.Size = new System.Drawing.Size(508, 150);
+            this.dataGridViewStock.TabIndex = 26;
+            this.dataGridViewStock.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewStock_CellClick);
+            // 
+            // textBoxDateRecorded
+            // 
+            this.textBoxDateRecorded.Location = new System.Drawing.Point(317, 373);
+            this.textBoxDateRecorded.Name = "textBoxDateRecorded";
+            this.textBoxDateRecorded.Size = new System.Drawing.Size(127, 22);
+            this.textBoxDateRecorded.TabIndex = 41;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(219, 376);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 16);
+            this.label4.TabIndex = 40;
+            this.label4.Text = "DateRecorded";
+            // 
+            // FormStock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxDateRecorded);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxQuantity);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonBook);
+            this.Controls.Add(this.buttonAuthor);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.textBoxISBN);
+            this.Controls.Add(this.textBoxAuthorID);
+            this.Controls.Add(this.textBoxStockID);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ISBN);
+            this.Controls.Add(this.dataGridViewStock);
+            this.Name = "FormStock";
+            this.Text = "FormStock";
+            this.Load += new System.EventHandler(this.FormStock_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStock)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxQuantity;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonBook;
+        private System.Windows.Forms.Button buttonAuthor;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.TextBox textBoxISBN;
+        private System.Windows.Forms.TextBox textBoxAuthorID;
+        private System.Windows.Forms.TextBox textBoxStockID;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label ISBN;
+        private System.Windows.Forms.DataGridView dataGridViewStock;
+        private System.Windows.Forms.TextBox textBoxDateRecorded;
+        private System.Windows.Forms.Label label4;
+    }
+}
